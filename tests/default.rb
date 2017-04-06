635404a1afba8d070ee02fefd7592acb57e1a3a6
Darren Win <dwin1000test@gmail.com>
2017-04-05 17:18:45 -0700

    Fixed Inspec tests to pass apache2

--- tests/default.rb
+++ tests/default.rb
@@ -1,14 +1,14 @@
-describe port(8080) do
-  it { should be_listening }
-  skip 'This is an example test, replace with your own test.'
-end
-
 if os[:family] == 'debian'
     describe package 'apache2' do
         it { should be_installed }
     end
 end
 
+describe port(80) do
+  it { should be_listening }
+  #skip 'This is an example test, replace with your own test.'
+end
+
 describe command 'curl localhost' do
-    its('stdout') { should match /Hello World/ }
+    its('stdout') { should match /Debian original for Ubuntu/ }
 end
